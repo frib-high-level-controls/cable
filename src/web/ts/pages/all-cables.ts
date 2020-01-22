9550e954541f75624863d9715b1c73ab67f9933c
Dylan Maxwell <maxwelld@frib.msu.edu>
2020-01-22 13:13:55 -0500

    Clean up and test all-cables page

--- src/web/ts/pages/all-cables.ts
+++ src/web/ts/pages/all-cables.ts
@@ -1,14 +1,9 @@
 /**
  * All Cables tables page
  */
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'jquery-ui/themes/base/all.css';
+import './base';
 
-import '@fortawesome/fontawesome-free/js/all';
-
-import 'popper.js';
-
-import 'bootstrap';
+import 'datatables.net-bs4/css/dataTables.bootstrap4.min.css';
 
 import 'jquery-ui/ui/widgets/autocomplete';
 import 'jquery-ui/ui/widgets/datepicker';
@@ -24,18 +19,10 @@ import 'datatables.net-buttons/js/buttons.print.min.js';
 
 import * as $ from 'jquery';
 
-import * as moment from 'moment';
-
 import * as dtutil from '../shared/datatablesutil';
 
 import {
-  json2List,
-  nameAuto,
-} from '../lib/util';
-
-import {
   ajax401,
-  disableAjaxCache,
 } from '../lib/ajaxhelper';
 
 import {
@@ -43,62 +30,40 @@ import {
 } from '../lib/barchart';
 
 import {
-  approvedByColumn,
-  approvedOnColumn,
   approvedOnLongColumn,
   basicColumns,
   commentsColumn,
   conduitColumn,
-  createdOnColumn,
-  detailsLinkColumn,
-  editLinkColumn,
   filterEvent,
-  fnDeselect,
-  fnGetSelected,
-  fnSelectAll,
-  fnSetDeselect,
   fnUnwrap,
   fnWrap,
-  formatCableStatus,
   fromColumns,
   highlightedEvent,
   lengthColumn,
   numberColumn,
-  obsoletedByColumn,
-  obsoletedOnLongColumn,
   ownerProvidedColumn,
-  rejectedByColumn,
-  rejectedOnColumn,
   requestNumberColumn,
-  requiredColumn,
   sButtons,
   sDom2InoF,
-  selectColumn,
   selectEvent,
   statusColumn,
   submittedByColumn,
-  submittedOnColumn,
-  tabShownEvent,
   toColumns,
-  updatedOnColumn,
   updatedOnLongColumn,
   versionColumn,
 } from '../lib/table';
 
-/*global fnAddFilterFoot: false, oTableTools: false, filterEvent: false, numberColumn: false, requestNumberColumn: false, statusColumn: false, versionColumn:falase, updatedOnLongColumn: false, approvedOnLongColumn: false, submittedByColumn:false, basicColumns: false, ownerProvidedColumn, fromColumns: false, toColumns: false,conduitColumn, lengthColumn, commentsColumn: false, sDom2InoF: false, fnAddFilterHead: false, fnWrap: false, fnUnwrap: false, barChart: false, selectEvent: false, highlightedEvent: false*/
-/*global ajax401: false*/
-/*global window: false*/
 
-$(function () {
+$(() => {
 
   ajax401('');
 
   const readyTime = Date.now();
-
-  var allAoColumns = ([numberColumn, requestNumberColumn, statusColumn, versionColumn, updatedOnLongColumn, approvedOnLongColumn, submittedByColumn] as Array<any>).concat(basicColumns.slice(0, 2), basicColumns.slice(3, 8), ownerProvidedColumn, fromColumns, toColumns).concat([conduitColumn, lengthColumn, commentsColumn]);
+  // tslint:disable:max-line-length
+  const allAoColumns = ([numberColumn, requestNumberColumn, statusColumn, versionColumn, updatedOnLongColumn, approvedOnLongColumn, submittedByColumn] as any[]).concat(basicColumns.slice(0, 2), basicColumns.slice(3, 8), ownerProvidedColumn, fromColumns, toColumns).concat([conduitColumn, lengthColumn, commentsColumn]);
   let allTableWrapped = true;
 
-  var allTable = $('#all-cable').DataTable({
+  const allTable = $('#all-cable').DataTable({
     ajax: {
       url: basePath + '/allcables/json',
       dataSrc: '',
@@ -107,16 +72,16 @@ $(function () {
     processing: true,
     lengthMenu: [
       [25, 50, 100, 500, 1000, -1],
-      [25, 50, 100, 500, 1000, 'All']
+      [25, 50, 100, 500, 1000, 'All'],
     ],
     language: {
-      loadingRecords: 'Please wait - loading data from the server ...'
+      loadingRecords: 'Please wait - loading data from the server ...',
     },
     columns: allAoColumns,
     order: [
       [4, 'desc'],
       [5, 'desc'],
-      [0, 'desc']
+      [0, 'desc'],
     ],
     dom: sDom2InoF,
     buttons: sButtons,
@@ -129,27 +94,27 @@ $(function () {
   });
 
   dtutil.addFilterHead('#all-cable', allAoColumns);
-  // fnAddFilterFoot('#all-cable', allAoColumns);
 
   $('#all-cable').on('init.dt', () => {
+    // tslint:disable:no-console
     console.log('All table initialized: ' + String((Date.now() - readyTime) / 1000) + 's' );
   });
 
-  $('#all-wrap').click(function () {
+  $('#all-wrap').click(() => {
     allTableWrapped = true;
     fnWrap(allTable);
   });
 
-  $('#all-unwrap').click(function () {
+  $('#all-unwrap').click(() => {
     allTableWrapped = false;
     fnUnwrap(allTable);
   });
 
-  $('#reload').click(function () {
+  $('#reload').click(() => {
     allTable.ajax.reload();
   });
 
-  $('#bar').click(function () {
+  $('#bar').click(() => {
     barChart(allTable);
   });
 
