68a8223292185bda0a72b217aa9ac497d720e8bd
Dylan Maxwell <maxwelld@frib.msu.edu>
2020-01-23 16:11:42 -0500

    Cleanup and test user profile page

--- src/web/ts/pages/profile.ts
+++ src/web/ts/pages/profile.ts
@@ -8,29 +8,28 @@ import * as $ from 'jquery';
 import * as moment from 'moment';
 
 
-$(function(){
-  var sub = $('#sub').prop('checked');
-  $('#modify').click(function(e){
+$(() => {
+  let sub = $('#sub').prop('checked');
+  $('#modify').click((e) => {
     if ($('#sub').prop('checked') === sub) {
       $('#message').append('<div class="alert alert-info"><button class="close" data-dismiss="alert">x</button>The subscription state was not changed.</div>');
     } else {
       sub = $('#sub').prop('checked');
-      var request = $.ajax({
+      const request = $.ajax({
         // url: 'profile',
         type: 'PUT',
         async: true,
         data: JSON.stringify({subscribe: sub}),
         contentType: 'application/json',
         processData: false,
-        dataType: 'json'
-      }).done(function(json) {
-        var timestamp = request.getResponseHeader('Date');
-        var dateObj = moment(timestamp);
+        dataType: 'json',
+      }).done((json) => {
+        const timestamp = request.getResponseHeader('Date');
+        const dateObj = moment(timestamp);
         $('#message').append('<div class="alert alert-info"><button class="close" data-dismiss="alert">x</button>The modification was saved at ' + dateObj.format('HH:mm:ss') + '.</div>');
-      }).fail(function(jqXHR, status, error) {
+      }).fail((jqXHR, status, error) => {
         // TODO change to modal
         alert('The save request failed. You might need to try again or contact the admin.');
-      }).always(function() {
       });
     }
     e.preventDefault();
